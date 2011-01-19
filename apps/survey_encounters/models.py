9959a0aeaed88e90020bb01a55fef14930a36791
Allen <allen.machary@gmail.com>
2011-01-19 14:39:59 +0300

    process xform submission linking questionaire qns to answer and forms move process func to model ***

--- apps/survey_encounters/models.py
+++ apps/survey_encounters/models.py
@@ -10,7 +10,15 @@ from reporters.models import Reporter
 
 from survey_outlets.models import Outlet
 from survey_locations.models import Region, District
-from survey_encounters.processor import add_encounter
+#from survey_encounters.processor import add_encounter
+import httplib, urllib
+from threading import Thread
+#from datetime import datetime
+from django.contrib.gis.geos import Point
+
+#from reporters.models import Reporter
+from hq.models import ReporterProfile
+
 
 
 class Question(models.Model):
@@ -19,6 +27,7 @@ class Question(models.Model):
     '''    
     question = models.CharField(max_length = 256)
     answer_type = models.CharField(max_length = 10,)
+    xmlPath = models.CharField(max_length = 256)
     date_added = models.DateTimeField(default = datetime.now)
     
     def __unicode__(self):
@@ -40,13 +49,16 @@ class Form(models.Model):
         submitted form data
     '''
     date_received = models.DateTimeField(default = datetime.now)
-    date_taked = models.DateTimeField()
+    date_taken = models.DateTimeField()
     survey_user = models.ForeignKey(Reporter)
-    outlet = models.ForeignKey(Outlet)
+    outlet = models.ForeignKey(Outlet, null=True, blank=True)
     # datetime for form save
     
     def __unicode__(self):
-        d = '%s : %s' % (self.outlet.outlet_name, self.date_received)
+        if self.outlet:
+            d = '%s : %s' % (self.outlet.outlet_name, self.date_received)
+        else:
+            d = 'None : %s' % (self.date_received)
         return d
 
 class QALink(models.Model):
@@ -58,6 +70,104 @@ class QALink(models.Model):
     def __unicode__(self):
         return 'Form %s : Q %s - A %s' % (self.form, self.question, self.answer)
     
+
+
+
+
+def add_encounter(sender, instance, created, **kwargs): #get sender, instance, created
+    """ Adds survey encounters submission from xforms to database """
+    # only process newly created forms, not all of them
+    if not created: return
+    print ">>>>>>>>>>> FORMS ;-) <<<<<<<<<<<<<<<"
+    
+    # check the form type to see if it is a new sample
+    form_xmlns = instance.formdefmodel.target_namespace
+    data = instance.formdefmodel.row_as_dict(instance.raw_data)
+
+    form = create_form(data)
+    thread = Thread( target = add_data, args =(data, form))
+    thread.start()
+    
+def create_form(data):
+    """ Create a form for submitted xform """
+    # Check if the reporter is have a profile
+    alias = data["data_interviewer"] #TODO: get reporter username from jr username tag
+    consent = data["data_consent_intervieweeconsent"]
+    try:
+        reporter = Reporter.objects.get(alias__iexact = alias)
+        # make sure the reporter have a profile for a domain.
+        # TODO: Limit the submission to a domain
+        reporter_profile = ReporterProfile.objects.get(reporter=reporter)
+        # save a reporter with a profile
+    except Exception, e:
+        raise
+    
+    # check if consent was given or not and process
+    in_form = Form()
+    if  int(consent) == 1:
+        # check if outlet exist
+        o_name = data["data_outletname"]
+        outlet = Outlets.objects.get(outlet_name__iexact = o_name)
+        # if not create an outlet
+        if outlet == []:
+            outlet = Outlet()
+            outlet.outlet_name = o_name
+            outlet.owner_name = 'Mwenyeduka'
+            outlet.type = data["data_outletType"]
+            outlet.point = Point(-7.00,40.00)
+            outlet.save()
+        in_form.outlet = outlet
+    else:
+        # process deny submissions
+        print "HEREEEEEEE else----"
+    
+    in_form.survey_user = reporter
+    in_form.date_taken = datetime.now()
+    in_form.save()
+    
+    return in_form
+
+def add_data(data_list, form):   
+    """ Add field data to answers and links questionare qn to submits """ 
+    # take each data from list and match with a Question
+    # create a list of only tags with actual data reference
+    data = []
+    IGNORE = ['id', 
+              'data interviewer', 
+              'data_region', 
+              'data_district', 
+              'data_village', 
+              'data_location',
+              ]
+    for d in data_list:
+        if data_list[d] != None:
+            if not d in IGNORE:
+                data.append(d)
+                
+    qns = Question.objects.filter(xmlPath__in = data)
+    for d in data:
+        # get question
+        for qn in qns:
+            if qn.xmlPath == d: 
+                # store answer
+                answer = Answer()
+                answer.answer = data_list[d]
+                answer.save()
+                
+                # link questions and answer
+                QALink(
+                       question = qn,
+                       answer = answer,
+                       form = form,
+                       ).save()
+                break
+
+
+def test(sender, instance, created, **kwargs):
+    ''' testing django signal actually works '''
+    print ">>>>TESTING DJANGO SIGNALS<<<<<"
+
+
 
 # Register to receive signals each time a Metadata is saved    
 post_save.connect(add_encounter, sender=Metadata)
--- apps/survey_encounters/processor.py
+++ apps/survey_encounters/processor.py
@@ -3,6 +3,7 @@ import httplib, urllib
 from datetime import datetime
 from django.contrib.gis.geos import Point
 
+from reporters.models import Reporter
 from hq.models import ReporterProfile
 
 def add_encounter(sender, instance, created, **kwargs): #get sender, instance, created
@@ -21,37 +22,53 @@ def add_encounter(sender, instance, created, **kwargs): #get sender, instance, c
 def create_form(data):
     """ Create a form for submitted xform """
     print " [[[[ CREATE FORM ]]]]] "
-
-    # TODO: check if outlet is in db else create the outlet
-#    o_name =  data[data_outlet]
-#    if o_name:
-#        outlet = Outlet.objects.get(outlet_name__iexact = o_name)
-#    else:
-#        outlet = Outlet(outlet_name = o_name,
-#                        owner_name = "Aptana",
-#                        point = Point(-7.00, 40.00)
-#                        ).save()
-    print "Outlet -----> %s" % ("outlet",)
+    
     # Check if the reporter is have a profile
-    alias = sample_data["data_interviewer"] #TODO: get reporter username from jr username tag
+    alias = data["data_interviewer"] #TODO: get reporter username from jr username tag
+    print "++++++++++> %s" % (alias,)
+    consent = data["data_consent_intervieweeconsent"]
+    print '****************** %s' % (consent,)
     try:
         reporter = Reporter.objects.get(alias__iexact = alias)
         # make sure the reporter have a profile for a domain.
         # TODO: Limit the submission to a domain
         reporter_profile = ReporterProfile.objects.get(reporter=reporter)
+        print "+++++++++++> %s" %(reporter,)
         # save a reporter with a profile
     except Exception, e:
         raise
     
+#    form = Form()
+    # check if consent was given or not and process
+    in_form = Form()
+    print '****************** %s' % (consent,)
+    if  int(consent) == 1:
+        # check if outlet exist
+        o_name = data["data_outletname"]
+        print "HEREEEEEEE"
+        outlet = Outlets.objects.get(outlet_name__iexact = o_name)
+        # if not create an outlet
+        if outlet == []:
+            print "HEREEEEEEE 2"
+            outlet = Outlet()
+            outlet.outlet_name = o_name
+            outlet.owner_name = 'Mwenyeduka'
+            outlet.type = data["data_outletType"]
+            outlet.point = Point(-7.00,40.00)
+            outlet.save()
+        in_form.outlet = outlet
+    else:
+        # process deny submissions
+        print "HEREEEEEEE else----"
+    
+    print "HEREEEEEEE 3"
+    in_form.survey_user = reporter
+    in_form.date_taken = datetime.now()
+    in_form.save()
+    print "HEREEEEEEE 4"
     print "Reporter -------------> %s" %(reporter,)
-    # create form
-    form =Form(
-                outlet = outlet,
-                survey_user = reporter,
-                date_taken = datetime.now(),
-                ).save()
-    print "----------> %s" % (form,)
-    return form
+    print "FORM ----------> %s" % (in_form,)
+#    return form
 
 def add_data(data_list, form):   
     """ Add field data to answers and links questionaire qn to submits """ 
